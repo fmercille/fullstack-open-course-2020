6ec24ed32ce98426ea90b2606295a4099d13228f
Frederic Mercille <fmercille@gmail.com>
2020-07-08 22:54:57 -0400

    Exercise 3.11

--- part2/phonebook/src/services/entries.js
+++ part2/phonebook/src/services/entries.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = 'https://quiet-brushlands-01945.herokuapp.com/api/persons'
+const baseURL = '/api/persons'
 
 const getAll = () => {
   return axios.get(baseURL)
